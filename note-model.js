5099501e3d5ad27d21b0c3656c7d6c979346e3a5
Sam Pritchard <sampritchard@msn.com>
2017-06-19 17:11:54 +0100

    created method to return text; tested

--- note-model.js
+++ note-model.js
@@ -1,7 +1,13 @@
 (function(exports){
+
   function Note(text) {
     this.text = text;
   };
 
+  Note.prototype.checkText = function () {
+    return this.text;
+  };
+
   exports.Note = Note;
+
 })(this);
--- note-test.js
+++ note-test.js
@@ -1,6 +1,11 @@
 function testNoteHasText() {
   var note = new Note("Hello World!");
-  assert.isTrue(note.text === "Hello World!!");
+  assert.isTrue(note.text === "Hello World!");
 };
-
 testNoteHasText()
+
+function testNoteCheckTextMethod() {
+  var note = new Note("Hello again!");
+  assert.isEqual(note.checkText(), "Hello again!");
+};
+testNoteCheckTextMethod();
